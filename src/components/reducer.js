59205db22115e4b117493330163039c659bef9ce
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-10-03 18:47:46 +0530

    modified:   src/components/reducer.js

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -13,14 +13,13 @@
 
 // export default countReducer;
 
-const counterReducer = (state = 1, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case "INCREMENT":
       return state + 1;
     case "DECREMENT":
       return state - 1;
-    case "RESET":
-      return (state = 0);
+
     default:
       return state;
   }
